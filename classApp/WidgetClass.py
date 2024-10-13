0c41f92c3ced70e25da1d860f0f196baa511cbf3
Astrou Lui <luisdaniel16an@gmail.com>
2024-10-13 08:37:25 -0400

    feat: adding the class for `Textfield`

--- classApp/WidgetClass.py
+++ classApp/WidgetClass.py
@@ -1,17 +1,21 @@
 import yaml
 import flet as ft
 
+#Lectura del archivo config
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
 
 # VARIABLES GLOBALES
     # ALINIAMIENTO
-COLOR_PRIMARY = config['colors']['primary']
-COLOR_SECOND = config['colors']['second']
-    # COLORES DE LA APP
 ALIGN_VERT = ft.MainAxisAlignment.CENTER
 ALIGN_HOR = ft.CrossAxisAlignment.CENTER
+    # COLORES DE LA APP
+COLOR_PRIMARY = config['colors']['primary']
+COLOR_SECOND = config['colors']['second']
+    # OTROS
+STATE = ft.ControlState
 
+#CLASE BOTON
 class Button(ft.ElevatedButton):
     def __init__(self, text, click_action):
         super().__init__()
@@ -19,8 +23,21 @@ class Button(ft.ElevatedButton):
         self.bgcolor= COLOR_SECOND
         self.color= COLOR_PRIMARY
         self.on_click = click_action
-
-
+        self.style = ft.ButtonStyle(
+            color={
+                STATE.HOVERED: COLOR_SECOND,
+                STATE.FOCUSED: COLOR_PRIMARY,
+                STATE.DEFAULT: COLOR_PRIMARY
+            },
+            bgcolor={
+                STATE.HOVERED: COLOR_PRIMARY,
+                STATE.DEFAULT: COLOR_SECOND,
+            },
+            side={
+                STATE.HOVERED: ft.BorderSide(2, COLOR_SECOND),
+            }
+        )
+#CLASE View
 class ViewClass(ft.View):
     def __init__(self, router, controls):
         super().__init__()
@@ -30,3 +47,20 @@ class ViewClass(ft.View):
         self.vertical_alignment = ALIGN_VERT
         self.horizontal_alignment = ALIGN_HOR
 
+#CLASE TextField
+class Field(ft.Container):
+    def __init__(self, label):
+        super().__init__()
+        self.content=ft.Column([
+            ft.Text(value=label, color=COLOR_SECOND),
+            ft.TextField(
+                bgcolor=COLOR_PRIMARY, 
+                cursor_color=COLOR_SECOND, 
+                color=COLOR_SECOND, 
+                border_color=COLOR_SECOND,
+                text_align= ALIGN_VERT,
+                text_size=config['input']['text-size'],
+                height=config['input']['height']
+            )
+        ], spacing=5)
+        self.padding=10
